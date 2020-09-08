0f89665a075373d1ccbb39e963a26cac39717ebe
victor-mouquin <victor.mouquin@gmail.com>
2020-09-08 19:21:00 +0200

    Add files via upload

---
+++ stations.py
@@ -0,0 +1,96 @@
+stations1 = [
+'Vaughan Metropolitan Centre',
+'Highway 407',
+'Pioneer Village',
+'York University',
+'Finch West',
+'Downsview',
+'Sheppard West',
+'Wilson',
+'Yorkdale',
+'Lawrence West',
+'Glencairn',
+'Eglinton West',
+'St Clair West',
+'Dupont',
+'Spadina',
+'St George',
+'Museum',
+"Queen's Park",
+'St Patrick',
+'Osgoode',
+'St Andrew',
+'Union',
+'King',
+'Queen',
+'Dundas',
+'College',
+'Wellesley',
+'Bloor-Yonge',
+'Rosedale',
+'Summerhill',
+'St Clair',
+'Davisville',
+'Eglinton',
+'Lawrence',
+'York Mills',
+'Sheppard-Yonge',
+'North York Centre',
+'Finch']
+
+
+
+stations2 = [
+'Kipling',
+'Islington',
+'Royal York',
+'Old Mill',
+'Jane',
+'Runnymede',
+'High Park',
+'Keele',
+'Dundas West',
+'Lansdowne',
+'Dufferin',
+'Ossington',
+'Christie',
+'Bathurst',
+'Spadina',
+'St George',
+'Bay',
+'Bloor-Yonge',
+'Sherbourne',
+'Castle Frank',
+'Broadview',
+'Chester',
+'Pape',
+'Donlands',
+'Greenwood',
+'Coxwell',
+'Woodbine',
+'Main Street',
+'Victoria Park',
+'Warden',
+'Kennedy']
+
+stations3 = [
+'Kennedy',
+'Lawrence East',
+'Ellesmere',
+'Midland',
+'Scarborough Centre',
+'McCowan']
+
+stations4 = [
+'Sheppard-Yonge',
+'Bayview',
+'Bessarion',
+'Leslie',
+'Don Mills']
+
+
+
+stations = stations1 + stations2 + stations3 + stations4
+ttc = [station + ' Station Toronto' for station in stations]
+
+
